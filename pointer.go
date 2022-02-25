b6101e77736173865d87a55f57f0b7ad22e76c10
depri11 <wirlandikadevri@gmail.com>
2022-02-25 10:59:50 +0700

    25 februari 2022

--- pointer.go
+++ pointer.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type Address struct {
+	City, Province, Country string
+}
+
+func ChangeCountryToIndonesia(address *Address) {
+	address.Country = "Indonesia"
+}
+
 func main() {
 	var numberA int = 4
 	var numberB *int = &numberA
@@ -17,5 +25,14 @@ fmt.Println("numberA (value)   :", numberA)
 fmt.Println("numberA (address) :", &numberA)
 fmt.Println("numberB (value)   :", *numberB)
 fmt.Println("numberB (address) :", numberB)
+
+var alamat = Address {
+	City: "Subang",
+	Province: "Jawa Barat",
+	Country: "",
+}
+
+ChangeCountryToIndonesia(&alamat)
+fmt.Println(alamat)
 	
 }
